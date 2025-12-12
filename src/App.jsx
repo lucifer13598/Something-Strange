3d801dbb82e538fb3b1ee7dc2bbdcca02ed4a788
lucifer13598 <lucifer13598@gmail.com>
2025-12-12 23:48:31 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import Heros from './components/Heros.jsx'
+import About from './components/About.jsx'
+import Navbar from './components/Navbar.jsx'
+import Features from './components/Features.jsx'
+import Story from './components/Story.jsx'
+import Contact from './components/Contact.jsx'
+import Footer from './components/Footer.jsx'
+
+
+const App = () => {
+  return (
+    <main className='relative min-h-screen w-screen 
+    overflow-x-hidden bg-yellow-300'>
+      <Navbar  />
+      <Heros/>
+    <About />
+    <Features/>
+    <Story />
+    <Contact/>
+    <Footer/>
+
+
+    </main>
+  )
+}
+
+export default App
+
+ 
